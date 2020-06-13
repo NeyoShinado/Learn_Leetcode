ae113b2bbfc67cd518dafdcd24609b50e2904e1c
Neyo <13411984510@163.com>
2020-06-13 15:29:35 +0800

    #100 6/13

--- 437_path_sum.py
+++ 437_path_sum.py
@@ -58,7 +58,7 @@ def traverse(node, sum):
 
 '''
 # Version 1
-# 递归实现
+# 双层递归
 # TC: O(N^2), SC: O(N)
 # 每条子路径都重复计算了一遍
 class Solution:
@@ -81,9 +81,60 @@ def countpath(root, sum):
 '''
 
 
+'''
 # Version 2
 # TS: O(N), SC: O(N)
-# 前缀和 + 哈希表
+# 前缀和 + DFS + 哈希表 + 回溯
 # 前缀和指到达当前元素路径上的元素和，两个元素前缀和currSum相等，意味着中间节点的和为0
 # 同理，若AB节点的currSum相差target，A到B之间元素之和为target
 #*注：前缀和需要两节点间只有一条路径，适用于无环图
+class Solution:
+	def pathSum(self, root, sum):
+		mp = {}
+		mp[0] = 1
+		res = self.dfs(root, 0, mp, sum)
+		
+		return res
+
+	def dfs(self, root, path_sum, mp, sum):
+		if not root:
+			return 0
+		# 更新当前pathsum
+		path_sum += root.val
+		# 先查询符合值再更新路径和表，避免sum=0 时出错
+		res = 0 if (path_sum - sum) not in mp.keys() else mp[path_sum - sum]
+		# 更新路径和表，并给子树递归
+		mp[path_sum] = 1 if (path_sum) not in mp.keys() else mp[path_sum]+1
+		res += self.dfs(root.left, path_sum, mp, sum)
+		res += self.dfs(root.right, path_sum, mp, sum)
+		#！子树遍历完后要清除当前的前缀和，避免回溯到父节点后影响上一层
+		# 因为当前前缀和是不属于前缀的
+		mp[path_sum] -= 1
+
+		return res
+'''
+
+
+# Version 3
+# 回溯搜索
+# TC: O(NlogN), SC: O(N)
+class Solution:
+	def pathSum(self, root, sum):
+		def path_sum(root, target, map, curend):
+			if not root:
+				return 0
+
+			cursum = 0
+			res = 0
+			map[curend] = root.val
+			for i in range(curend, -1, -1):
+				cursum += map[i]
+				if cursum == target:
+					res += 1
+
+			res += path_sum(root.left, target, map, curend+1)
+			res += path_sum(root.right, target, map, curend+1)
+			return res
+
+		map = [0] * 1000
+		return path_sum(root, sum, map, 0)
